e27f3ea6585c5d7664e06e4807072d7db556dd0f
Mark Evans and Oliver Nightingale <bambinos+markevans+olivernn@new-bamboo.co.uk>
2011-01-28 17:37:28 +0000

    autoload view classes

--- lib/walrus.rb
+++ lib/walrus.rb
@@ -4,3 +4,13 @@ require 'walrus/asset_helper'
 
 ActionView::Template.register_template_handler(:mustache, Walrus::Handler)
 ActionView::Base.send :include, Walrus::AssetHelper
+
+module Walrus
+  class Railtie < Rails::Railtie
+
+    config.before_configuration do |app|
+      app.config.autoload_paths += %W(#{app.config.root}/app/views)
+    end
+
+  end
+end
--- lib/walrus/handler.rb
+++ lib/walrus/handler.rb
@@ -7,12 +7,10 @@ module Walrus
     def compile(template)
       view_path = "#{template.virtual_path}_view"
       abs_view_path = Rails.root.join('app/views', view_path)
-      if File.exists? "#{abs_view_path}.rb"
-        require abs_view_path
-        view_class = view_path.sub('/_','/').classify.constantize
-        Rails.logger.debug(view_class)
-      else
-        view_class = Walrus::View
+      view_class = begin
+        view_path.classify.constantize
+      rescue NameError => e
+        Walrus::View
       end
       "#{view_class}.new(self, '#{template.source.gsub(/'/, "\\\\'")}').render.html_safe"
     end
