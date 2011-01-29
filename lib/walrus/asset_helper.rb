7a72da438c2310702ac4777f980dc66798ff1fcc
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-01-29 19:49:37 +0000

    added template include tag

--- lib/walrus/asset_helper.rb
+++ lib/walrus/asset_helper.rb
@@ -2,9 +2,9 @@ module Walrus
   module AssetHelper
     def template_include_tag(*sources)
       sources.collect do |source|
-        template_path = Rails.root.join('app/views', source)
+        template_path = Rails.root.join('app/views', controller_name, "_#{source}.html.mustache")
         template = File.open(template_path, "rb")
-        content_tag :script, template.read, :type => "text/mustache"
+        content_tag :script, template.read.html_safe, :type => "text/mustache", :id => "#{source.dasherize}-template"
       end.join("\n").html_safe
     end
   end
