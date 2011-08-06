87fbe2e3c49206cfd08a4e240842a31b289c105d
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-08-06 20:01:35 +0100

    only include an engine if this is rails 3.1 or above

--- lib/poirot.rb
+++ lib/poirot.rb
@@ -6,7 +6,7 @@ ActionView::Template.register_template_handler(:mustache, Poirot::Handler)
 ActionView::Base.send :include, Poirot::AssetHelper
 
 module Poirot
-  class Engine < Rails::Engine ; end
+  class Engine < Rails::Engine ; end if Rails.version >= "3.1"
   class Railtie < Rails::Railtie
     config.before_configuration do |app|
       app.config.autoload_paths += %W(#{app.config.root}/app/views)
