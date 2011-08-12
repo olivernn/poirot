75bb402411fb9c5301005dd2e4e0756b5be3867f
Ismael Celis <ismaelct@gmail.com>
2011-08-12 12:06:57 +0100

    template_include_tag helper resolves paths starting with "/" to absolute view folders

--- lib/poirot/asset_helper.rb
+++ lib/poirot/asset_helper.rb
@@ -2,10 +2,21 @@ module Poirot
   module AssetHelper
     def template_include_tag(*sources)
       sources.collect do |source|
-        template_path = Rails.root.join('app/views', controller_name, "_#{source}.html.mustache")
-        template = File.open(template_path, "rb")
-        content_tag :script, template.read.html_safe, :type => "text/mustache", :id => "#{source.dasherize}-template"
+        template = File.open(_poirot_resolve_partial_path(source), "rb")
+        content_tag :script, template.read.html_safe, :type => "text/mustache", :id => "#{source.parameterize}-template"
       end.join("\n").html_safe
     end
+    
+    def _poirot_resolve_partial_path(source)
+      if source.to_s =~ /^\// # absolute path to a different view folder
+        segments = source.to_s.split('/')
+        partial_name = "_#{segments.pop}.html.mustache"
+        segments << partial_name
+        Rails.root.join('app/views', *segments)
+      else
+        Rails.root.join('app/views', controller_name, "_#{source}.html.mustache")
+      end
+    end
+    
   end
 end
