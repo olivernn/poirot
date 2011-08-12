caeeb4d19ec45d4942ab06de5f1996ebec384c7d
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-08-12 12:44:41 +0100

    remove deprecated modules and super classes

--- lib/poirot/handler.rb
+++ lib/poirot/handler.rb
@@ -1,10 +1,6 @@
 module Poirot
-  class Handler < ActionView::Template::Handler
-    include ActionView::Template::Handlers::Compilable
-
-    self.default_format = :mustache
-
-    def compile(template)
+  class Handler
+    def self.call(template)
       view_path = "#{template.virtual_path}_view"
       abs_view_path = Rails.root.join('app/views', view_path)
       view_class = begin
